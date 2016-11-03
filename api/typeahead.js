c06cb855579992619dc28b6bcb22e13a5223cf57
mkausas <mkausas@gmail.com>
2016-11-02 22:14:38 -0400

    Added enhancment view with initial styling completed

--- api/typeahead.js
+++ api/typeahead.js
@@ -7,40 +7,6 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 var sample = '<div style=" overflow: hidden; width: 316px; height: 94px; border-color: black; border-width: 1px; border-style: solid;">     <img src="https://d.ibtimes.co.uk/en/full/1356835/number-2-u-s-president-barack-obama-second-most-admired-person-planet.jpg?w=400" alt="Smiley face" height="42" width="42" style="     max-width: 117px;     max-height: 117px;     width: auto;     height: auto;     float: left;">       <div style="     width: 200px;     display: inline-block;     padding-left: 20px;     position: relative;">          <div style="padding-top: 14px">         <span style="         font-family: &quot;Avenir Next&quot;;         font-size: 20px; color:#515151;">Marty Kausas</span>          <br>          <span style="font-family: &quot;Avenir Next&quot;;font-size: 13px; color:#515151;">Developer @              <a href="https://mixmax.com/" target="_blank" style="text-decoration:none; color: #A014BE;">Mixmax</a>         </span>          <br>            <a style="font-family: &quot;Avenir Next&quot;; font-size: 13px; color: #428CFF;">Add to Contacts</a>         </div>     </div> </div>';
 
-// readTextFile("mixmax.html");
-
-// function setSample(text) {
-//   sample = [text, text];
-// }
-
-// function readTextFile(file) {
-//   var rawFile = new XMLHttpRequest();
-//   rawFile.open("GET", file, false);
-//   var allText = "";
-//   rawFile.onreadystatechange = function () {
-//     if(rawFile.readyState === 4) {
-//       if(rawFile.status === 200 || rawFile.status == 0) {
-//         setSample(rawFile.responseText);
-//         allText = rawFile.responseText;
-//       }
-//     }
-//   }
-//   rawFile.send(null);
-// }
-
-function SignatureCard() {
-
-  this.imageURL = "";
-  this.name = "";
-  this.role = "";
-  this.place = "";
-  this.placeURL = "";
-
-  // links contain map of types
-  this.links = {"github_url": null, "resume_link": nill};
-
-}
-
 // The Type Ahead API.
 module.exports = function(req, res) {
   var term = req.query.text.trim();
@@ -75,11 +41,7 @@ module.exports = function(req, res) {
     return;
   }
 
-  var results = _.chain([2, 3])//response.body.data)
-    // .reject(function(image) {
-    //   return !image || !image.images || !image.images.fixed_height_small;
-    // })
-    .map(function(image) {
+  var results = _.chain([2, 3]).map(function(image) {
       return {
         title: sample,
         text: 'stuff'//'http://giphy.com/' + image.id
